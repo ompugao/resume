e2c8747e9d7fc2b5bd2a74dea7b5ced2832c7169
Shohei Fujii <fujii.shohei@gmail.com>
2026-01-26 20:44:02 +0900

    wip

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,26 @@
-const themeToggle = document.getElementById('theme-toggle');
-const html = document.querySelector('html');
+var themeToggleBtn = document.getElementById('theme-toggle');
 
-themeToggle.addEventListener('click', () => {
-    html.classList.toggle('dark');
-    localStorage.setItem('theme', html.classList.contains('dark') ? 'dark' : 'light');
+themeToggleBtn.addEventListener('click', function() {
+
+    // if set via local storage previously
+    if (localStorage.getItem('color-theme')) {
+        if (localStorage.getItem('color-theme') === 'light') {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        }
+
+    // if NOT set via local storage previously
+    } else {
+        if (document.documentElement.classList.contains('dark')) {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        } else {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        }
+    }
+    
 });
-
-if (localStorage.getItem('theme') === 'dark') {
-    html.classList.add('dark');
-}
---
+++ content/education/_index.md
@@ -0,0 +1,19 @@
+---
+title: "Education"
+weight: 1
+---
+
+---
+
+### Ph.D. in Robotics
+**Nanyang Technological University, Mechanical and Aerospace Engineering** | _08/2020 - 07/2024_
+
+Thesis: [GPU-accelerated real-time motion planning for safe human-robot collaboration](https://doi.org/10.32657/10356/178799)
+
+
+### M.S. in Information Science and Technology
+**the University of Tokyo, Graduate School of Information Science and Technology** | _04/2013 - 03/2015_
+
+
+### B.S. in Engineering
+**the University of Tokyo, Graduate School of Information Science and Technology** | _04/2009 - 03/2013_
